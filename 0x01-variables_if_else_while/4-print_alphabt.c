b7487658a46553fb12d8c6d588a2b8ffc33e4fc9
Ayodeji Oni <ayodejioni1505@gmail.com>
2023-02-28 12:05:36 -0800

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char start;
+
+	start = 'a';
+	for (i = start; i != '{'; i++)
+	{
+		if (i == 'e' || i == 'e')
+		{
+			continue;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
