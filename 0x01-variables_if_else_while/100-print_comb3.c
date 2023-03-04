b769cd61f0081d0f7c5ad5ee744e7102b5ae251e
Ayodeji Oni <ayodejioni1505@gmail.com>
2023-03-04 11:54:37 -0800

    new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,21 @@
  */
 int main(void)
 {
-	int i;
-	int start;
+	int i, j;
 
-	start = '0';
-	for (i = start; i != ':'; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
-	putchar('\n');
 	return (0);
 }
 
