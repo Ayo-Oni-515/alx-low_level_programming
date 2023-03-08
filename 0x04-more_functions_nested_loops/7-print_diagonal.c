a9dd7d0f304ede60ab8ab6347ff4398ab7c1938d
Ayodeji Oni <ayodejioni1505@gmail.com>
2023-03-07 21:48:48 -0800

    new

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * print_diagonal - check the code
+  * @n: Argument
+  * Return: -1, 0 or 1
+  */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
