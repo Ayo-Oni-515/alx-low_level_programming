916f9f8b4edc9295ce1188bad626d523420eba30
Ayodeji Oni <ayodejioni1505@gmail.com>
2023-03-11 22:11:55 -0800

    new

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,17 +8,17 @@ int main(void)
 {
 	int i;
 	unsigned long fibonacci[100] = {0, 1, 1, 2};
-	unsigned long sum;
+	long sum;
 
 	for (i = 4; i <= 100; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 	}
 
-	for (i = 2; fibonacci[i] <= 4000000 ; i++)
+	for (i = 2; fibonacci[i] < 4000000 ; i++)
 	{
 		sum += fibonacci[i];
 	}
-	printf("%lu\n", sum);
+	printf("%ld\n", sum);
 	return (0);
 }
